77691e24c50815cd174478ce6a433ec47894400b
btocarmona2021 <btocarmona@gmail.com>
2025-10-28 21:14:00 -0300

    Repository and first view

--- src/django/aulas/views.py
+++ src/django/aulas/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse as res
 
 # Create your views here.
+
+# Hola esto es una prueba de textos
+
+def hello(req):
+    return res("<h1>Hola Mundo</h1>")
