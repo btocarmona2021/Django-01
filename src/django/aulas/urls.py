b657000d83bc946b7ae9572f585dc033aafd545d
btocarmona2021 <btocarmona@gmail.com>
2025-11-03 11:22:17 -0300

    templates

--- src/django/aulas/urls.py
+++ src/django/aulas/urls.py
@@ -1,8 +1,13 @@
 
 from django.urls import path
-from .views import hello
+from .views import hello ,bye,edad,primer_plantila,segunda_plantilla,tercer_plantilla
 
 urlpatterns = [
     path('hello/', hello),
-
+    path('bye/', bye),
+    path("edad/<int:anios>/<int:futuro>", edad),
+    path("plantilla1",primer_plantila),
+    path("plantilla2",segunda_plantilla),
+    path("plantilla3",tercer_plantilla)
 ]
+
--- src/django/aulas/views.py
+++ src/django/aulas/views.py
@@ -1,9 +1,54 @@
 from django.shortcuts import render
 from django.http import HttpResponse as res
+from datetime import date as dt
+from django.template import Template, Context
+from django.template.loader import get_template
+from django.shortcuts import render
 
-# Create your views here.
-
-# Hola esto es una prueba de textos
 
 def hello(req):
     return res("<h1>Hola Mundo</h1>")
+
+
+def bye(req):
+    return res("<h1>Hasta luego</h1>")
+
+
+def edad(req, anios, futuro):
+    anio_actual = dt.today().year
+    incremento = futuro - anio_actual
+    cumplira = anios + incremento
+    return res("en el anio %d cumpliras %d anios" % (futuro, cumplira))
+
+
+def primer_plantila(req):
+    plantilla = """
+    
+    """
+
+    tpl = Template(plantilla)
+    ctx = Context({"nombre":"Alberto Carmona"})
+    mensaje = tpl.render(ctx)
+
+    return res(mensaje)
+
+
+def segunda_plantilla(req):
+    tpl = get_template("segunda_pantilla.html")
+    ctx = {
+        "nombre": "Santiago Carmona",
+        "fecha_actual":dt.today(),
+        'titulo':'mi sitio web en django',
+        }
+    mensaje = tpl.render(ctx)
+
+    return res(mensaje)
+
+def tercer_plantilla(req):
+
+    ctx = {
+        "nombre": "Santiago Carmona",
+        "fecha_actual": dt.today(),
+        "titulo": "mi sitio web en django",
+    }
+    return render(req,"tercer_plantilla.html",ctx)
